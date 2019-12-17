bef5e37b1a1e9e9926fd17373a554a92ec380f9f
luojh <623597383@qq.com>
2019-12-17 16:33:47 +0800

    http 解决跨域，新增git忽略配置

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const Koa = require('koa')
 const app = new Koa()
 const port = 8081;
-const router = require('koa-router')();
-const db = require('./db/db')
 var cors = require('koa-cors');
-
+const router = require('./src/routers');
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
   ctx.set('Access-Control-Allow-Origin', '*');
@@ -19,12 +17,13 @@ app.use(async (ctx, next) => {
 app.use(
   cors({
     origin: function (ctx) { //设置允许来自指定域名请求
-      const whiteList = ['http://127.0.0.1:8081', 'http://localhost:8081']; //可跨域白名单
-      let url = ctx.header.referer.substr(0, ctx.header.referer.length - 1);
+      const whiteList = ['http://127.0.0.1:8081', 'http://localhost:8081']; //可跨域白名单   
+      //允许浏览器上直接地址（无referer）
+      let url = ctx.header.referer ? ctx.header.referer.substr(0, ctx.header.referer.length - 1) : '';
       if (whiteList.includes(url)) {
         return url //注意，这里域名末尾不能带/，否则不成功，所以在之前我把/通过substr干掉了
       }
-      return 'http://localhost::3000' //默认允许本地请求3000端口可跨域
+      return `http://localhost::${port}` //默认允许本地请求3000端口可跨域
     },
     maxAge: 5, //指定本次预检请求的有效期，单位为秒。
     credentials: true, //是否允许发送Cookie
@@ -32,19 +31,8 @@ app.use(
     allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
   })
-);
-router.get('/item/page', async (ctx, next) => {
-  // ctx.
-  var sql = 'select * FROM item_type;'
-  const result = await db(sql);
-  console.log(ctx);
-  ctx.response.body = 'hello word'
-});
-router.get('/', async (ctx, next) => {
-  ctx.response.body = '<h1>Index</h1>';
-});
+)
 app.use(cors())
-
 app
   .use(router.routes())
   .use(router.allowedMethods());
--- src/routers.js
+++ src/routers.js
@@ -1,7 +1,16 @@
 const router = require('koa-router')();
-router.get('/item-type',async (ctx,next)=>{
-  var params = ctx.params;
-  console.log(params);
-
-})
-module.exports = router.routes();
+const db = require('../db/db')
+router.get('/item/page', async (ctx, next) => {
+  // ctx.
+  var sql = 'select * FROM item_type;'
+  const result = await db(sql);
+  ctx.response.body = {
+    data: result,
+    success: true,
+    errMsg: '111',
+  }
+});
+router.get('/', async (ctx, next) => {
+  ctx.response.body = '<h1>Index</h1>';
+});
+module.exports = router;
