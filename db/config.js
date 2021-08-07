edbedd9bf11e98712595deef08bab0795e79894f
luojh <623597383@qq.com>
2021-08-07 16:26:43 +0800

    调试 enhanced-resolve

--- db/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  database: 'time_mamage',
-  user: 'root',
-  password: 'db123456',
-  host: 'localhost',  
-}
---
+++ index.js
@@ -0,0 +1,4 @@
+const Resolve = require('enhanced-resolve').ResolverFactory
+const lookupStartPath = '/Users/luojh/my_project/server-template'
+
+const resolve = Resolve.createResolver
